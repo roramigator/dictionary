1e3d107d04923e0ef9c01584476e5ea724d55a7c
Morado <roramigator@gmail.com>
2020-09-27 23:40:34 -0400

    update api config

--- src/api/rc.js
+++ src/api/rc.js
@@ -1,14 +1,9 @@
 const base_url = "https://api.dictionaryapi.dev/api/v2/entries/";
 
-const languages = {
-  default: "en",
-  english: "en",
-};
-
 export default (word, language = "en") => {
   return new Promise((resolve, reject) => {
     try {
-      fetch(`http://localhost:9000/dictionary?lan=${language}&txt=${word}`)
+      fetch(`/dictionary?lan=${language}&txt=${word}`)
         .then((r) => r.json())
         .then((r) => resolve(r));
     } catch (e) {
