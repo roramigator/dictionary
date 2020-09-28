0197e6a82dca46444cfa231a6c06e85717a8a8cd
Morado <roramigator@gmail.com>
2020-09-28 00:18:48 -0400

    ...we don't know devops

--- src/api/rc.js
+++ src/api/rc.js
@@ -1,21 +1,9 @@
 const base_url = "https://api.dictionaryapi.dev/api/v2/entries";
 
-// export default (word, language = "en") => {
-//   return new Promise((resolve, reject) => {
-//     try {
-//       fetch(`http://localhost:9000/dictionary?lan=${language}&txt=${word}`)
-//         .then((r) => r.json())
-//         .then((r) => resolve(r));
-//     } catch (e) {
-//       reject(e);
-//     }
-//   });
-// };
-
 export default (word, language = "en") => {
   return new Promise((resolve, reject) => {
     try {
-      fetch(`${base_url}/${language}/${word}`)
+      fetch(`/dictionary?lan=${language}&txt=${word}`)
         .then((r) => r.json())
         .then((r) => resolve(r));
     } catch (e) {
